b48cd73d6ee34c996a6e5e767b8acca88da5a234
Dwsok472 <sok472@naver.com>
2025-03-28 15:35:37 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: "globalThis",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8082", // Spring 서버 주소
+        changeOrigin: true,
+      },
+      "/ws": {
+        target: "http://localhost:8082", // Spring 서버 주소
+        changeOrigin: true,
+        ws: true, // 웹소켓 처리 설정
+      },
+    },
+  },
 })
