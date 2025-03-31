d431645545878795ffb098484b33d2213c957631
NBear32 <hansel32153@gmail.com>
2025-03-31 12:13:13 +0900

    .

---
+++ src/components/Register/RegisterStep2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Octagon from './Octagon'
+
+function RegisterStep2() {
+    return (
+        <Container>
+            <H1>회원가입</H1>
+            <Octagon />
+            <Octagon />
+        </Container>
+    )
+}
+
+export default RegisterStep2
