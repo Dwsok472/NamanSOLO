3180304874a79807c504bb0831b1ec07cc219fa5
NBear32 <hansel32153@gmail.com>
2025-03-31 09:18:41 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,8 @@ function App() {
               />
               <Route path="/find-id" element={<Find isFindId={true} />} />
               <Route path="/find-pwd" element={<Find isFindId={false} />} />
-              <Route path="/follow" element={<Follow />} />
+              <Route path="/follower" element={<Follow type={follower} />} />
+              <Route path="/following" element={<Follow type={following} />} />
             </Routes>
           </MainContent>
           <Footer />
