ced8eea365a92ff742ed6ab64e22720aae5847eb
JJ0824 <auxjungjae@gmail.com>
2025-04-01 18:19:57 +0900

    update

--- src/components/MyPage/todo/Detailtodo.jsx
+++ src/components/MyPage/todo/Detailtodo.jsx
@@ -12,7 +12,7 @@ const CardWrap = styled.div`
 `;
 
 const Card = styled.div`
-  height: 400px;
+  height: 300px;
   background-color: white;
   border-radius: 50px;
   border: 1px solid #3333;
@@ -52,18 +52,12 @@ const Info = styled.div`
   font-size: 1rem;
 `;
 
-const ImagePreview = styled.img`
-  width: 80px;
-  height: 80px;
-  object-fit: cover;
-  border-radius: 8px;
-  margin-right: 8px;
-`;
-
-const ImageRow = styled.div`
-  display: flex;
-  gap: 8px;
-  flex-wrap: wrap;
+const ColorPreview = styled.div`
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  border: 2px solid #ccc;
 `;
 
 const ButtonRow = styled.div`
@@ -84,33 +78,29 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-function TravelDetailView({ event, onClose, onEdit }) {
+function Detailtodo({ event, onClose, onEdit }) {
   if (!event) return null;
 
   return (
     <CardWrap>
-      <Card>
-        <Top>
-            <IconEdit onClick={onEdit} /> 수정
-            <TopX onClick={onClose}>
-                <IconClose />
-            </TopX>
-            여행 일정
-        </Top>
-        <Bottom>
-          <Info>{event.title}</Info>
-          <Info>{event.startDate} ~ {event.endDate}</Info>
-          {event.images?.length > 0 && (
-            <ImageRow>
-              {event.images.map((img, idx) => (
-                <ImagePreview key={idx} src={URL.createObjectURL(img)} alt="여행사진" />
-              ))}
-            </ImageRow>
-          )}
-        </Bottom>
-      </Card>
+        <Card>
+            <Top>
+                <Button onClick={onEdit}>
+                    <IconEdit />
+                </Button>
+                <TopX onClick={onClose}>
+                    <IconClose />
+                </TopX>
+                기념일
+            </Top>
+            <Bottom>
+                <Info>{event.title}</Info>
+                <Info>{event.date}</Info>
+                <ColorPreview color={event.color} />
+            </Bottom>
+        </Card>
     </CardWrap>
   );
 }
 
-export default TravelDetailView;
+export default Detailtodo;
