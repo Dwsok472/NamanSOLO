905f75b7f12cef61f9d0cd08ca6fac8ef2b96c28
NBear32 <hansel32153@gmail.com>
2025-04-02 18:16:50 +0900

    .

--- src/components/Album/LeftBox.jsx
+++ src/components/Album/LeftBox.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { IconSearch } from '../Icons'
+
 
 const Container = styled.div`
     border: 1px solid black;
@@ -12,20 +15,70 @@ const Container = styled.div`
     gap: 10px;
 `
 const TopBox = styled.div`
-     border: 1px solid black;
+     /* border: 1px solid black; */
      width: 100%;
      border-radius: 16px;
-     height: 50px;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     .logo{
+        font-size: 2.5rem;
+        font-weight: 700;
+        color:#ff9987;
+     }
+     .button{
+        font-weight: 700;
+        font-size: 0.7rem;
+        background-color: white;
+        border: 1px solid #ff9987;
+        border-radius: 20px;
+        width: 80px;
+        height: 30px;
+        color: #2b2b2b;
+        margin: 5px;
+        &:hover{
+            background-color: #ff9987;
+            color: white;
+        }
+     }
 `
-const NewestButton = styled.button``
-const LikedButton = styled.button``
-const CommentedButton = styled.button``
 const MiddleBox = styled.div`
-     border: 1px solid black;
      width: 100%;
      border-radius: 16px;
-     height: 150px;
 `
+const SearchBox = styled.div`
+width: 90%;
+height: 50px;
+border: 1px solid #6d6d6d33;
+border-radius: 30px;
+display: flex;
+align-items: center;
+margin: 0 auto;
+background-color: #bbbbbb;
+`;
+const InputBox = styled.div`
+width: 95%;
+height: 40px;
+display: flex;
+align-items: center;
+
+`
+    ;
+const Input = styled.input`
+outline:none;
+border: none;
+width: 100%;
+border: 1px solid #3333;
+border-radius: 30px;
+height: 100%;
+margin-left: 10px;
+padding-left: 10px;
+margin-right: 5px;
+&::placeholder{
+  font-size: 1rem;
+  font-weight: 700;
+}
+`;
 const BottomBox = styled.div`
      border: 1px solid black;
      width: 100%;
@@ -34,14 +87,37 @@ const BottomBox = styled.div`
 `
 
 function LeftBox() {
+    const [inputKeyword, setInputKeyword] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    const urlKeyword = new URLSearchParams(location.search).get('username');
     return (
         <Container>
             <TopBox>
-                <NewestButton>최신순</NewestButton>
-                <LikedButton>좋아요순</LikedButton>
-                <CommentedButton>댓글순</CommentedButton>
+                <div className='logo'>WeARE</div>
+                <button className='button' id='newest'>최신순</button>
+                <button className='button' id='like'>좋아요순</button>
+                <button className='button' id='comment'>댓글순</button>
             </TopBox>
-            <MiddleBox></MiddleBox>
+            <MiddleBox>
+                <SearchBox>
+                    <InputBox>
+                        <Input
+                            type="text"
+                            value={inputKeyword}
+                            onChange={(e) => setInputKeyword(e.target.value)}
+                            placeholder="USERNAME을 입력해주세요"
+                        />
+                        <IconSearch
+                            onClick={() => {
+                                inputKeyword
+                                    ? navigate(`/search?username=${inputKeyword}`)
+                                    : alert('검색어를 입력해주세요');
+                            }}
+                        />
+                    </InputBox>
+                </SearchBox>
+            </MiddleBox>
             <BottomBox></BottomBox>
         </Container>
     )
