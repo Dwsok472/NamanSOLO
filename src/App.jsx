102c0a94e1c5878d5b093b042b2eec069a68ba71
kjunwo <kangjunwoo02@naver.com>
2025-04-04 19:20:32 +0900

    app수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import styled, { createGlobalStyle } from 'styled-components';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Login from './components/Login/Login';
-import Find from './components/FindIdAndPwd/Find';
-import Event from './components/Event';
-import RegisterMain from './components/Register/RegisterMain';
-import AllStories from './components/Story/AllStories';
-import MainPage from './components/MainPage';
-import MyPage from './components/MyPage/MyPage';
-import MainMap from './components/Map/MainMap';
-import AlbumBoard from './components/Album/AlbumBoard';
-import AdminFeedPage from './components/Admin/AdminFeedPage';
-import AdminUserPage from './components/Admin/AdminUserPage';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./components/Login/Login";
+import Find from "./components/FindIdAndPwd/Find";
+import Event from "./components/Event";
+import RegisterMain from "./components/Register/RegisterMain";
+import AllStories from "./components/Story/AllStories";
+import MainPage from "./components/MainPage";
+import MyPage from "./components/MyPage/MyPage";
+import MainMap from "./components/Map/MainMap";
+import AlbumBoard from "./components/Album/AlbumBoard";
+import AdminFeedPage from "./components/Admin/AdminFeedPage";
+import AdminUserPage from "./components/Admin/AdminUserPage";
 
 const AppWrapper = styled.div`
   width: 100vw;
@@ -22,36 +22,18 @@ const AppWrapper = styled.div`
 `;
 
 const MainContent = styled.main.attrs(() => ({
-  id: 'main-content',
+  id: "main-content",
 }))`
   flex: 1;
-  padding-top: 75px;
+  padding-top: ${(props) => (props.noPadding ? "0" : "75px")};
 `;
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-}
-@font-face {
-    font-family: 'GmarketSansMedium';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-@font-face {
-    font-family: 'SUIT-Regular';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
-    font-weight: normal;
-    font-style: normal;
-}
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: GmarketSansMedium;
+    font-family: Poppins;
   }
 
   a {
@@ -76,12 +58,13 @@ const GlobalStyle = createGlobalStyle`
     pointer-events: auto !important;
     user-select: auto !important;
   }
-
 `;
 
 function AppRoutes() {
   const location = useLocation();
-  const isMainPage = location.pathname === "/";
+  const hiddenHeaderFooterRoutes = ["/", "/login"]; // 여기에 제외할 경로 추가
+
+  const isMainPage = hiddenHeaderFooterRoutes.includes(location.pathname);
 
   return (
     <AppWrapper>
@@ -91,23 +74,23 @@ function AppRoutes() {
         <Header
           logoText="WeARE"
           menuItems={[
-            { to: '/story/all', label: '전체 스토리' },
-            { to: '/map', label: '맵' },
-            { to: '/events', label: '이벤트' },
+            { to: "/story/all", label: "전체 스토리" },
+            { to: "/map", label: "맵" },
+            { to: "/events", label: "이벤트" },
           ]}
           subMenuItems={[
-            { to: '/mypage/info', label: '커플 정보' },
-            { to: '/mypage/story', label: '나의 스토리' },
-            { to: '/mypage/comment', label: '나의 댓글' },
-            { to: '/mypage/todo', label: '캘린더' },
-            { to: '/mypage/other', label: '그 외' },
+            { to: "/mypage/info", label: "커플 정보" },
+            { to: "/mypage/story", label: "나의 스토리" },
+            { to: "/mypage/comment", label: "나의 댓글" },
+            { to: "/mypage/todo", label: "캘린더" },
+            { to: "/mypage/other", label: "그 외" },
           ]}
           loginText="로그인"
           signupText="회원가입"
         />
       )}
 
-      <MainContent>
+      <MainContent noPadding={isMainPage}>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<Login />} />
@@ -122,8 +105,7 @@ function AppRoutes() {
           <Route path="/find-pwd" element={<Find isFindId={false} />} />
         </Routes>
       </MainContent>
-
-      <Footer />
+      {!isMainPage && <Footer />}
     </AppWrapper>
   );
 }
