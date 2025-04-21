6c7a66b1b07541ea710bcfebbf19630386b12b66
YooSeoyoung <yoo_seoyoung@naver.com>
2025-04-22 00:11:06 +0900

    이벤트 수정

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,17 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { fetchFemalePresents, fetchMalePresents } from './api2';
+import gift1 from './img/gift1.jpg';
+import gift2 from './img/gift2.jpg';
+import gift3 from './img/gift3.jpg';
 
 const Container = styled.div`
   padding: 40px 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin: 0;
+  padding: 0;
+  position: relative;
 `;
 
+const ImgWrap = styled.div`
+  display: flex;
+  cursor: pointer;
+  width: 100%;
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100vh;
+    object-fit: contain;
+  }
+`;
+const Right = styled.div`
+  width: 100%;
+`;
 const Title = styled.h1`
   text-align: center;
-  font-size: 4rem;
-  color : #1f0606;
+  font-size: 3.5rem;
+  color: #1f0606;
   margin-top: 40px;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
 `;
 
 const CategoryContainer = styled.div`
@@ -19,25 +41,32 @@ const CategoryContainer = styled.div`
   justify-content: center;
   gap: 32px;
   flex-wrap: wrap;
+  overflow-y: auto;
+  height: 850px;
+  &::-webkit-scrollbar {
+    width: 7px; /* 세로 스크롤바의 너비를 8px로 설정 */
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #727272; /* 핸들의 색상 */
+    border-radius: 10px;
+  }
 `;
 
 const Category = styled.div`
   width: 100%;
-  max-width: 800px;
+  max-width: 650px;
   height: fit-content;
-  border-radius: 20px;
   padding: 28px 24px;
-  background-color: ${(props) =>
-    props.$gender === 'male' ? '#9dc4ff7d' : '#ffd7cc99' };
+  /* background-color: ${(props) =>
+    props.$gender === 'male' ? '#9dc4ff7d' : '#ffd7cc99'}; */
   box-shadow: 0 3px 10px rgba(200, 200, 200, 0.15);
 `;
 
 const SubTitle = styled.h2`
-  font-size: 1.4rem;
+  font-size: 2rem;
   margin-bottom: 20px;
-  font-weight: 500;
-  color: ${(props) =>
-    props.$gender === 'male' ? '#3c97ff' : '#f88479' };
+  font-weight: 700;
+  color: ${(props) => (props.$gender === 'male' ? '#000000' : '#f88479')};
   text-align: center;
   background-color: none;
   padding: 8px 12px;
@@ -55,7 +84,7 @@ const GiftList = styled.ul`
 `;
 
 const GiftItem = styled.li`
-  width: 48%;
+  width: 45%;
   border: 1px solid #e3e3e3;
   border-radius: 12px;
   margin-bottom: 18px;
@@ -74,33 +103,30 @@ const GiftItem = styled.li`
     display: flex;
     flex-direction: column;
     height: 100%;
-      &:hover {
+    &:hover {
       font-weight: 700;
     }
   }
   img {
     width: 100%;
-    min-height: 280px;
-    max-height: 280px;
     object-fit: cover;
     border-radius: 12px;
   }
 
   h3 {
     height: 72px;
-    font-size: 1.0rem;
-    margin: 12px 0 8px;
+    font-size: 0.8rem;
     color: #333;
     text-align: center;
     display: -webkit-box;
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow: hidden;
-    text-overflow: ellipsis; 
+    text-overflow: ellipsis;
   }
 
   p {
-    font-size: 0.95rem;
+    font-size: 0.7rem;
     margin-top: auto;
     color: #666;
     white-space: nowrap;
@@ -109,11 +135,13 @@ const GiftItem = styled.li`
   }
 `;
 
+const images = [gift1, gift2, gift3];
 function Event() {
   const [maleGifts, setMaleGifts] = useState([]);
   const [femaleGifts, setFemaleGifts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentImage, setCurrentImage] = useState(0);
 
   const stripHTML = (html) => {
     const div = document.createElement('div');
@@ -126,8 +154,8 @@ function Event() {
       try {
         const [maleData, femaleData] = await Promise.all([
           fetchMalePresents(),
-          fetchFemalePresents()
-        ])
+          fetchFemalePresents(),
+        ]);
 
         console.log('👩 여자 선물:', femaleData);
         console.log('👨 남자 선물:', maleData);
@@ -144,43 +172,66 @@ function Event() {
     fetchGifts();
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   if (loading) return <div>로딩 중...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <Container>
-      <Title>선물 랭킹</Title>
-      <CategoryContainer>
-        <Category $gender="male">
-          <SubTitle $gender="male">남자 선물</SubTitle>
-          <GiftList>
-            {Array.isArray(maleGifts) && maleGifts.map((gift, index) => (
-              <GiftItem key={index} title={stripHTML(gift.title)}>
-                <a href={gift.shoppingUrl} target="_blank" rel="noopener noreferrer">
-                  <img src={gift.imageUrl} alt={stripHTML(gift.title)} />
-                  <h3 dangerouslySetInnerHTML={{ __html: gift.title }} />
-                  <p>가격: {Number(gift.price).toLocaleString()}원</p>
-                </a>
-              </GiftItem>
-            ))}
-          </GiftList>
-        </Category>
+      <ImgWrap>
+        <img src={images[currentImage]} alt={`slide-${currentImage}`} />
+      </ImgWrap>
+      <Right>
+        <Title>선물 랭킹 TOP10</Title>
+        <CategoryContainer>
+          <Category $gender="male">
+            {/* <SubTitle $gender="male">남자 선물</SubTitle> */}
+            <GiftList>
+              {Array.isArray(maleGifts) &&
+                maleGifts.map((gift, index) => (
+                  <GiftItem key={index} title={stripHTML(gift.title)}>
+                    <a
+                      href={gift.shoppingUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={gift.imageUrl} alt={stripHTML(gift.title)} />
+                      <h3 dangerouslySetInnerHTML={{ __html: gift.title }} />
+                      <p>가격: {Number(gift.price).toLocaleString()}원</p>
+                    </a>
+                  </GiftItem>
+                ))}
+            </GiftList>
+          </Category>
 
-        <Category $gender="female">
-          <SubTitle $gender="female">여자 선물</SubTitle>
-          <GiftList>
-            {Array.isArray(femaleGifts) && femaleGifts.map((gift, index) => (
-              <GiftItem key={index} title={stripHTML(gift.title)}>
-                <a href={gift.shoppingUrl} target="_blank" rel="noopener noreferrer">
-                  <img src={gift.imageUrl} alt={stripHTML(gift.title)} />
-                  <h3 dangerouslySetInnerHTML={{ __html: gift.title }} />
-                  <p>가격: {Number(gift.price).toLocaleString()}원</p>
-                </a>
-              </GiftItem>
-            ))}
-          </GiftList>
-        </Category>
-      </CategoryContainer>
+          <Category $gender="female">
+            {/* <SubTitle $gender="female">여자 선물</SubTitle> */}
+            <GiftList>
+              {Array.isArray(femaleGifts) &&
+                femaleGifts.map((gift, index) => (
+                  <GiftItem key={index} title={stripHTML(gift.title)}>
+                    <a
+                      href={gift.shoppingUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={gift.imageUrl} alt={stripHTML(gift.title)} />
+                      <h3 dangerouslySetInnerHTML={{ __html: gift.title }} />
+                      <p>가격: {Number(gift.price).toLocaleString()}원</p>
+                    </a>
+                  </GiftItem>
+                ))}
+            </GiftList>
+          </Category>
+        </CategoryContainer>
+      </Right>
     </Container>
   );
 }
