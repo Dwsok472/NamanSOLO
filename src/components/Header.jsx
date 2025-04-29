dc1e459f13882b205598f259ab01fc9a56858331
JJ0824 <auxjungjae@gmail.com>
2025-04-29 11:40:17 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,18 +62,18 @@ function Header(props) {
   return (
     <>
       <Container $show={showHeader}>
-        {(!isSidebarOpen&& 
-          <>
-            <Hamburger onClick={() => setSidebarOpen(true)}>
-              <FontAwesomeIcon icon={faBars} />
-            </Hamburger>
-            <Link to="/">
-              <Logo ref={logoRef} $visible={showLogo !== false}>
-                {logoText}
-              </Logo>
-            </Link>
-          </>
-        )}
+      {(!isSidebarOpen&& 
+        <>
+          <Hamburger onClick={() => setSidebarOpen(true)}>
+            <FontAwesomeIcon icon={faBars} />
+          </Hamburger>
+          <Link to="/">
+            <Logo ref={logoRef} $visible={showLogo !== false}>
+              {logoText}
+            </Logo>
+          </Link>
+        </>
+      )}
       </Container>
 
       <Overlay $open={isSidebarOpen} onClick={closeSidebar} />
